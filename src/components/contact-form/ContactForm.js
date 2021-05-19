b43beae89dbc16baf91c8267251edb87ef040e6a
Pav Swies <pav.swies@dontbeshy.com>
2021-05-19 13:19:19 +0100

    Add names to form inputs

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -3,24 +3,22 @@ import * as style from './contact-form.module.css';
 
 const ContactForm = () => {
   return (
-    <form className={style.form} method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+    <form className={style.form} method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact" action="/thank-you">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
       <div className={style.formRow}>
         <label htmlFor="name">Your Name:</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" name="name" />
       </div>
       <div className={style.formRow}>
         <label htmlFor="email">Your Email:</label>
-        <input type="text" id="email" />
+        <input type="text" id="email" name="email" />
       </div>
       <div className={style.formRow}>
         <label htmlFor="message">Message:</label>
-        <textarea id="message"></textarea>
+        <textarea id="message" name="message"></textarea>
       </div>
-      <button class="cta form__button" type="submit">
-        Send message
-      </button>
+      <input class="cta form__button" type="submit" value="Send message" />
     </form>
   );
 };
